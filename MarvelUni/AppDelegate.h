b164a749da7e12b23f061bc5ccef4dddcdda2873
Kevin Almanza <jellyfish.ninja@gmail.com>
2012-11-25 20:57:59 -0600

    Initial Commit

---
+++ MarvelUni/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MarvelUni
+//
+//  Created by Kevin Almanza on 11/25/12.
+//  Copyright (c) 2012 Kevin Almanza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MarvelUni/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MarvelUni
+//
+//  Created by Kevin Almanza on 11/25/12.
+//  Copyright (c) 2012 Kevin Almanza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
